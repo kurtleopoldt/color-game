4f324b489386ce5bcd251687ca53dc4862cb2ca8
Kurt Leopoldt <kleopoldt@gmail.com>
2017-04-22 21:46:17 -0700

    added dropdown for color class selection. javascript interaction to be completed

--- assets/colorGame.js
+++ assets/colorGame.js
@@ -7,7 +7,7 @@ var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
-
+var colorSelection = "RGB";
 
 init();
 
@@ -15,6 +15,7 @@ function init(){
 	//mode buttons event listeners
 	setupModeButtons();
 	setupSqures();
+	colorDefSelect();
 	
 
 	reset();
@@ -74,35 +75,6 @@ function reset(){
 	h1.style.background = "steelblue";
 }
 
-// easyBtn.addEventListener("click", function(){
-// 	hardBtn.classList.remove("selected");
-// 	easyBtn.classList.add("selected");
-// 	numSquares = 3;
-// 	colors = generateRandomColors(numSquares);
-// 	pickedColor = pickColor();
-// 	colorDisplay.textContent = pickedColor;
-// 	for(var i = 0; i < squares.length; i++){
-// 		if(colors[i]){
-// 			squares[i].style.background = colors[i];
-// 		} else {
-// 			squares[i].style.display = "none";
-// 		}
-// 	}
-// });
-
-// hardBtn.addEventListener("click", function(){
-// 	hardBtn.classList.add("selected");
-// 	easyBtn.classList.remove("selected");
-// 	numSquares = 6;
-// 	colors = generateRandomColors(numSquares);
-// 	pickedColor = pickColor();
-// 	colorDisplay.textContent = pickedColor;
-// 	for(var i = 0; i < squares.length; i++){
-// 			squares[i].style.background = colors[i];
-// 			squares[i].style.display = "block";
-// 	}
-// });
-
 resetButton.addEventListener("click", function(){
 	reset();
 })
@@ -135,12 +107,25 @@ function generateRandomColors(num){
 	return arr;
 }
 
+//watch the colorDef selector
+function colorDefSelect() {
+	var colorDefChange = document.getElementById('colorDef');
+	colorDefChange.addEventListener('change', function() {
+  	return colorSelection = this.value;
+	}, false);
+}
+
+// add to this function for other varieties
 function randomColor(){
-	// pick a "red" from 0-255
-	var r = Math.floor(Math.random() * 256)
-	// pick a "green" from 0-255
-	var g = Math.floor(Math.random() * 256)
-	// pick a "blue" form 0-255
-	var b = Math.floor(Math.random() * 256)
-	return "rgb(" + r + ", " + g + ", " + b + ")";
+	if(colorSelection === "RGB") {
+		// pick a "red" from 0-255
+		var r = Math.floor(Math.random() * 256)
+		// pick a "green" from 0-255
+		var g = Math.floor(Math.random() * 256)
+		// pick a "blue" form 0-255
+		var b = Math.floor(Math.random() * 256)
+		return "rgb(" + r + ", " + g + ", " + b + ")";
+	} else {
+		console.log("cheese");
+	}
 }
